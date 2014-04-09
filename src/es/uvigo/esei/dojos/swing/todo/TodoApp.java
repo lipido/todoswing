ff21965eab349eb07b6ec58e6fd92a4489cb7ffe
lipido <lipido@gmail.com>
2014-04-09 21:04:20 +0200

    Add LookAndFeel configuration

--- src/es/uvigo/esei/dojos/swing/todo/TodoApp.java
+++ src/es/uvigo/esei/dojos/swing/todo/TodoApp.java
@@ -1,11 +1,32 @@
 package es.uvigo.esei.dojos.swing.todo;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import es.uvigo.esei.dojos.swing.todo.gui.MainWindow;
 
 public class TodoApp {
 
-	public static void main(String[] args) {
-		MainWindow window = new MainWindow();
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		configureLookAndFeel();
+		
+		MainWindow window = new MainWindow();		
 		window.setVisible(true);
+	}
+
+	private static void configureLookAndFeel() {
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("GTK+".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break; //preferred!
+		        }
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		        }
+		    }
+		} catch (Exception e) {		    
+		}
 	}
 }
